dfe9e80e9d8c7585ad8a67b62eb1f5f17d515d3f
Jon Hinks <projects.ws@jondh.me.uk>
2012-12-03 07:52:57 +0000

    Do real queries for test/encrypted comment counts

--- templates/_status.php
+++ templates/_status.php
@@ -3,7 +3,7 @@
 	<div class="inside">
 		<p>There are <?php echo $commentCount ?> comments in the blog, of which
 			<?php echo $encryptedCommentCount ?> are fully encrypted, and
-			<?php echo $bothCommentCount ?> are test-encrypted (i.e. the data is stored
+			<?php echo $testCommentCount ?> are test-encrypted (i.e. the data is stored
 			in plain text as well).</p>
 
 		<p>Of the encrypted comments, there are X separate keys in use (more than one is
--- templates/options.php
+++ templates/options.php
@@ -104,11 +104,12 @@
 			<li><s>Set up private key in cookie</s></li>
 			<li><s>Confirm priv key is saved</s></li>
 			<li><s>Create repo on Github</s></li>
+			<li><s>Status box to count plaintext/test/encrypted comments</s></li>
 			<li>Healthcheck: count number of pub keys used across all comments (if more than one, then report
 				it as a problem in UI)</li>
 			<ol>
 				<li>Have a few pub key hash chars per comment to achieve this?</li>
-				<li>Are there db calls to do counts of comments with certain metadata fields?</li>
+				<li><s>Are there db calls to do counts of comments with certain metadata fields?</s></li>
 			</ol>
 			<li><s>Admin banner to say when private cookie is detected</s></li>
 			<li>Speed tests to see if phpsec lib is faster/slower than openssl extension</li>
