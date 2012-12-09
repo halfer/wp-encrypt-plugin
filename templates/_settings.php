cb26a5e95c960f986bf3c697d5873b33efc7daff
Jon Hinks <projects.ws@jondh.me.uk>
2012-12-09 14:08:05 +0000

    Comment out settings we're not using/saving yet

--- templates/_settings.php
+++ templates/_settings.php
@@ -1,22 +1,28 @@
-<p>The first setting to decide on is whether to enable encryption. If it is turned on, you can either run
-	in test mode (where the original email and IP addresses are preserved) or in real mode, where they are
-	deleted. Once you are happy with the system, and are sure it works on your system, real mode is
-	recommended.
-</p>
+<form method="post">
+	
+	<?php /* Disabled, as we're not saving/loading these settings yet
+	<p>The first setting to decide on is whether to enable encryption. If it is turned on, you can either run
+		in test mode (where the original email and IP addresses are preserved) or in real mode, where they are
+		deleted. Once you are happy with the system, and are sure it works on your system, real mode is
+		recommended.
+	</p>
 
-<select>
-	<option value="off">Off</option>
-	<option value="test">Test mode (initially recommended)</option>
-	<option value="real">Real mode (be sure before you switch to this)</option>
-</select>
+	<select>
+		<option value="off">Off</option>
+		<option value="test">Test mode (initially recommended)</option>
+		<option value="real">Real mode (be sure before you switch to this)</option>
+	</select>
 
-<p>You may also set up a CPU load setting. This is set to slow initially, which is recommended
-	for shared hosting or if your web site performance would be adversely affected. Remember that if
-	you regularly consume a lot of CPU on shared hosts, you may be asked to upgrade to a VPS. If you're
-	not sure about this setting, leave it on slow.</p>
+	<p>You may also set up a CPU load setting. This is set to slow initially, which is recommended
+		for shared hosting or if your web site performance would be adversely affected. Remember that if
+		you regularly consume a lot of CPU on shared hosts, you may be asked to upgrade to a VPS. If you're
+		not sure about this setting, leave it on slow.</p>
 
-<select>
-	<option value="slow">Slow searches, low load</option>
-	<option value="medium">Faster searches, moderate load</option>
-	<option value="fast">Fast searches, high load</option>
-</select>
+	<select>
+		<option value="slow">Slow searches, low load</option>
+		<option value="medium">Faster searches, moderate load</option>
+		<option value="fast">Fast searches, high load</option>
+	</select>
+	*/ ?>
+
+</form>
