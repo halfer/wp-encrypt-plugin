bc78609c1b448503a57e891f0d0f6ba91e480a66
Jon Hinks <projects.ws@jondh.me.uk>
2012-12-09 18:21:23 +0000

    Remove settings on uninstall

--- lib/CommentsEncryptInit.php
+++ lib/CommentsEncryptInit.php
@@ -10,10 +10,9 @@ class CommentsEncryptInit
 		// Will be useful later on
 		$this->Main = $CommentsEncryptMain;
 
-		register_activation_hook(
-			CommentsEncryptMain::PATH_PLUGIN_NAME . '/main.php',
-			array($this, 'activationHook')
-		);
+		$pluginPath = CommentsEncryptMain::PATH_PLUGIN_NAME . '/main.php';
+		register_activation_hook($pluginPath, array($this, 'activationHook'));
+		register_uninstall_hook($pluginPath, array($this, 'uninstallHook'));
 	}
 
 	/**
@@ -23,5 +22,19 @@ class CommentsEncryptInit
 	{
 		// Will be ignored if it already exists, which is what we want
 		add_option(CommentsEncryptMain::OPTION_STORE_AVATAR_HASHES, true);
+	}
+
+	/**
+	 * Delete settings when we uninstall, not when we deactivate
+	 * 
+	 * @todo Can we double-check with the user if any comments are still encrypted?
+	 */
+	public function uninstallHook()
+	{
+		$ok =
+			delete_option(CommentsEncryptMain::OPTION_PUB_KEY) &&
+			delete_option(CommentsEncryptMain::OPTION_PUB_KEY_HASH);
+			delete_option(CommentsEncryptMain::OPTION_STORE_AVATAR_HASHES)
+		;
 	}
 }
