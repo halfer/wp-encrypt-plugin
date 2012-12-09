2ed385ab186ac8499d1ace46613dd0a54bdf4d70
Jon Hinks <projects.ws@jondh.me.uk>
2012-12-09 14:07:51 +0000

    UI wording change

--- templates/options.php
+++ templates/options.php
@@ -1,5 +1,5 @@
 <div id="encrypt-demo-options" class="wrap wp-comments-encrypt-page">
-	<h2>Comment Encryption Options</h2>
+	<h2>Comment Encryption Settings</h2>
 
 	<div class="metabox-holder">
 		<div class="meta-box-sortabless">
