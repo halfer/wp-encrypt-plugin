ea07d8ac9c3394c6839ca49a035b122ad85ad9ba
Jon Hinks <projects.ws@jondh.me.uk>
2012-12-29 20:43:06 +0000

    This commented-out code has been superceded

--- templates/_settings.php
+++ templates/_settings.php
@@ -1,18 +1,6 @@
 <form method="post">
 	
 	<?php /* Disabled, as we're not saving/loading these settings yet
-	<p>The first setting to decide on is whether to enable encryption. If it is turned on, you can either run
-		in test mode (where the original email and IP addresses are preserved) or in real mode, where they are
-		deleted. Once you are happy with the system, and are sure it works on your system, real mode is
-		recommended.
-	</p>
-
-	<select>
-		<option value="off">Off</option>
-		<option value="test">Test mode (initially recommended)</option>
-		<option value="real">Real mode (be sure before you switch to this)</option>
-	</select>
-
 	<p>You may also set up a CPU load setting. This is set to slow initially, which is recommended
 		for shared hosting or if your web site performance would be adversely affected. Remember that if
 		you regularly consume a lot of CPU on shared hosts, you may be asked to upgrade to a VPS. If you're
